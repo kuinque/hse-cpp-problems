f008af37fc4e479c33a1e449736d41fee495ce4c
quinque <krivetskyy@icloud.com>
2023-09-14 19:19:43 +0300

    fraction

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,10 +1,10 @@
 #include "reduce_fraction.h"
 #include <set>
 
-std::multiset<int64_t> FindPrimeDivisors(int64_t number){
+std::multiset<int64_t> FindPrimeDivisors(int64_t number) {
     std::multiset<int64_t> prime_divisors;
     for (int64_t i = 2; i * i <= number; ++i) {
-        while (number % i == 0){
+        while (number % i == 0) {
             number /= i;
             prime_divisors.insert(i);
         }
@@ -20,7 +20,7 @@ int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
     std::multiset<int64_t> denominator_divisors = FindPrimeDivisors(denominator);
     int64_t reduced_numerator = 1;
     int64_t reduced_denominator = 1;
-    while(!numerator_divisors.empty() || !denominator_divisors.empty()) {
+    while (!numerator_divisors.empty() || !denominator_divisors.empty()) {
         if (*numerator_divisors.begin() == *denominator_divisors.begin()) {
             numerator_divisors.erase(numerator_divisors.begin());
             denominator_divisors.erase(denominator_divisors.begin());
