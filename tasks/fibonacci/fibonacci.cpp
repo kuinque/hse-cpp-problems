f7574733aa960af43fe5b89189fdf4753d1419c2
quinque <krivetskyy@icloud.com>
2023-09-14 16:33:02 +0300

    fib

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,15 @@
 #include "fibonacci.h"
+#include <vector>
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    std::vector<int64_t> fibonacci(n);
+    if (n <= 2) {
+        return 1;
+    }
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
+    for (int64_t i = 2; i < n; ++i) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci[n - 1];
 }
