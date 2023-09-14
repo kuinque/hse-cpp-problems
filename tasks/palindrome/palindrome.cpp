7405f17ab235170bac070bd7f37b289b2a4c1067
quinque <krivetskyy@icloud.com>
2023-09-14 20:11:05 +0300

    lin

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,21 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    int64_t pointer1 = 0;
+    int64_t pointer2 = str.length() - 1;
+    while (pointer1 < pointer2) {
+        if (str[pointer1] == ' ') {
+            ++pointer1;
+            continue;
+        } else if (str[pointer2] == ' ') {
+            --pointer2;
+        }
+        if (str[pointer1] != str[pointer2]) {
+            return false;
+        } else {
+            ++pointer1;
+            --pointer2;
+        }
+    }
+    return true;
 }
