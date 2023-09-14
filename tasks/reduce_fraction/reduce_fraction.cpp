129ba05b26d744cf231d602f05bea259bb3f552d
quinque <krivetskyy@icloud.com>
2023-09-14 20:02:03 +0300

    gcd

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,36 +1,24 @@
 #include "reduce_fraction.h"
-#include <set>
 
-std::multiset<int64_t> FindPrimeDivisors(int64_t number) {
-    std::multiset<int64_t> prime_divisors;
-    for (int64_t i = 2; i * i <= number; ++i) {
-        while (number % i == 0) {
-            number /= i;
-            prime_divisors.insert(i);
+int64_t GCD(int64_t a, int64_t b) {
+    while (a > 0 && b > 0) {
+        if (a > b) {
+            a %= b;
+        } else {
+            b %= a;
         }
     }
-    if (number > 1) {
-        prime_divisors.insert(number);
+    if (a == 0) {
+        return b;
     }
-    return prime_divisors;
+    return a;
 }
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    std::multiset<int64_t> numerator_divisors = FindPrimeDivisors(numerator);
-    std::multiset<int64_t> denominator_divisors = FindPrimeDivisors(denominator);
-    int64_t reduced_numerator = 1;
-    int64_t reduced_denominator = 1;
-    while (!numerator_divisors.empty() || !denominator_divisors.empty()) {
-        if (*numerator_divisors.begin() == *denominator_divisors.begin()) {
-            numerator_divisors.erase(numerator_divisors.begin());
-            denominator_divisors.erase(denominator_divisors.begin());
-        } else if (*numerator_divisors.begin() < *denominator_divisors.begin()) {
-            reduced_numerator *= *numerator_divisors.begin();
-            numerator_divisors.erase(numerator_divisors.begin());
-        } else {
-            reduced_denominator *= *denominator_divisors.begin();
-            denominator_divisors.erase(denominator_divisors.begin());
-        }
+    while (GCD(numerator, denominator) != 1) {
+        int64_t temp = GCD(numerator, denominator);
+        numerator /= temp;
+        denominator /= temp;
     }
-    return reduced_numerator + reduced_denominator;
+    return numerator + denominator;
 }
