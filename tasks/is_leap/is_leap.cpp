87d7808ef09806a2365fed65bd4be5d298243183
quinque <krivetskyy@icloud.com>
2023-09-16 17:58:24 +0300

    leap leap

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,12 +1,7 @@
-#include <cstdint>
 #include "is_leap.h"
 
-const int16_t D1 = 4;
-const int16_t D2 = 100;
-const int16_t D3 = 400;
-
 bool IsLeap(int year) {
-    if ((year % D1 == 0 && year % D2 != 0) || year % D3 == 0) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
         return true;
     }
     return false;
