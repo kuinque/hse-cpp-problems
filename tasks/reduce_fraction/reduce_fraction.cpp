b0d32a3ace42bb6d423d97b6ea9527fad5c81152
quinque <krivetskyy@icloud.com>
2023-09-19 10:42:12 +0300

    .

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -2,10 +2,11 @@
 #include <numeric>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    while (std::gcd(numerator, denominator) != 1) {
-        int64_t temp = std::gcd(numerator, denominator);
-        numerator /= temp;
-        denominator /= temp;
+    int64_t calc_gcd = std::gcd(numerator, denominator);
+    while (calc_gcd != 1) {
+        numerator /= calc_gcd;
+        denominator /= calc_gcd;
+        calc_gcd = std::gcd(numerator, denominator);
     }
     return numerator + denominator;
 }
