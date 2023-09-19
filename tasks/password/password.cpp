d0d2de7ebffd8bd4f88a04be4ebeed375aad5878
quinque <krivetskyy@icloud.com>
2023-09-19 10:25:52 +0300

    .

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -5,39 +5,29 @@ bool ValidatePassword(const std::string& password) {
     static const char8_t MAXIMAL_ASCII = 126;
     static const size_t MINIMAL_PASSWORD_SIZE = 8;
     static const size_t MAXIMAL_PASSWORD_SIZE = 14;
-    static const int8_t MINIMAL_CLASS_COUNT = 3;
-    if (password.size() < MINIMAL_PASSWORD_SIZE || password.size() > MAXIMAL_PASSWORD_SIZE) {
+    static const int MINIMAL_CLASS_COUNT = 3;
+    if (std::clamp(password.size(), MINIMAL_PASSWORD_SIZE, MAXIMAL_PASSWORD_SIZE) != password.size()) {
         return false;
     }
+    int has_lower_letter = 0;
+    int has_upper_letter = 0;
+    int has_digit = 0;
+    int has_other_char = 0;
     for (const char8_t i : password) {
-        if (i < MINIMAL_ASCII || i > MAXIMAL_ASCII) {
+        if (std::clamp(i, MINIMAL_ASCII, MAXIMAL_ASCII) != i) {
             return false;
         }
-    }
-    bool has_lower_letter = false;
-    bool has_upper_letter = false;
-    bool has_digit = false;
-    bool has_other_char = false;
-    for (const char8_t i : password) {
-        if (std::isalpha(i)) {
-            if (std::islower(i)) {
-                has_lower_letter = true;
-            } else {
-                has_upper_letter = true;
-            }
+        if (std::islower(i)) {
+            has_lower_letter = 1;
+        } else if (std::isupper(i)) {
+            has_upper_letter = 1;
         } else if (std::isdigit(i)) {
-            has_digit = true;
+            has_digit = 1;
         } else {
-            has_other_char = true;
+            has_other_char = 1;
         }
     }
-    int8_t good_class_characters = 0;
-    for (bool i : {has_lower_letter, has_upper_letter, has_digit, has_other_char}) {
-        if (i) {
-            ++good_class_characters;
-        }
-    }
-    if (good_class_characters >= MINIMAL_CLASS_COUNT) {
+    if (has_lower_letter + has_upper_letter + has_digit + has_other_char >= MINIMAL_CLASS_COUNT) {
         return true;
     }
     return false;
