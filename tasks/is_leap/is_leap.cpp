88b38a9b520700f241ea4402fd7f13af6b117733
quinque <krivetskyy@icloud.com>
2023-09-19 10:00:03 +0300

    .

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -2,9 +2,9 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    static const int16_t LEAP_MOD_1_CRITERIA = 4;
-    static const int16_t LEAP_MOD_2_CRITERIA = 100;
-    static const int16_t LEAP_MOD_3_CRITERIA = 400;
+    static const int LEAP_MOD_1_CRITERIA = 4;
+    static const int LEAP_MOD_2_CRITERIA = 100;
+    static const int LEAP_MOD_3_CRITERIA = 400;
     if ((year % LEAP_MOD_1_CRITERIA == 0 && year % LEAP_MOD_2_CRITERIA != 0) || year % LEAP_MOD_3_CRITERIA == 0) {
         return true;
     }
