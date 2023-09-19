e2965cc87f686b681507435a62f0b00c74aed702
quinque <krivetskyy@icloud.com>
2023-09-19 10:10:41 +0300

    .

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -4,21 +4,21 @@ bool IsPalindrome(const std::string& str) {
     if (str.empty()) {
         return true;
     }
-    size_t pointer1 = 0;
-    size_t pointer2 = str.length() - 1;
-    while (pointer1 < pointer2) {
-        if (str[pointer1] == ' ') {
-            ++pointer1;
+    size_t left_pointer = 0;
+    size_t right_pointer = str.length() - 1;
+    while (left_pointer < right_pointer) {
+        if (str[left_pointer] == ' ') {
+            ++left_pointer;
             continue;
-        } else if (str[pointer2] == ' ') {
-            --pointer2;
+        } else if (str[right_pointer] == ' ') {
+            --right_pointer;
             continue;
         }
-        if (str[pointer1] != str[pointer2]) {
+        if (str[left_pointer] != str[right_pointer]) {
             return false;
         } else {
-            ++pointer1;
-            --pointer2;
+            ++left_pointer;
+            --right_pointer;
         }
     }
     return true;
