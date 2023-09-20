f25735001840a9af3a66dff9ab3c293f2dae72fe
quinque <krivetskyy@icloud.com>
2023-09-20 13:11:19 +0300

    .

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,5 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    return (slices_amount % height == 0 || slices_amount % width == 0) && height * width != slices_amount;
+    return (slices_amount % height == 0 || slices_amount % width == 0) && height * width > slices_amount;
 };
