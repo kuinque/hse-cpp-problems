5afaaf693c540cac9f6d832b473bb6440ccae207
quinque <krivetskyy@icloud.com>
2023-09-28 01:42:26 +0300

    нормально

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -1,7 +1,7 @@
 #include "admission.h"
+
 #include <unordered_map>
 #include <algorithm>
-
 
 bool operator<(const Date& left_date, const Date& right_date) {
     if (left_date.year < right_date.year) {
@@ -21,7 +21,8 @@ bool operator<(const Date& left_date, const Date& right_date) {
 }
 
 bool operator!=(const Date& left_date, const Date& right_date) {
-    return (left_date.day != right_date.day || left_date.month != right_date.month || left_date.year != right_date.year);
+    return (left_date.day != right_date.day || left_date.month != right_date.month ||
+            left_date.year != right_date.year);
 }
 
 bool operator<(const Student& left_student, const Student& right_student) {
@@ -46,17 +47,21 @@ bool operator>(const Applicant& left_applicant, const Applicant& right_applicant
 }
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
-    std::unordered_map <std::string, size_t> university_position;
+    std::unordered_map<std::string, size_t> university_position;
     std::vector<size_t> universities_student_count(universities.size());
     for (size_t pos = 0; pos < universities.size(); ++pos) {
         university_position[universities[pos].name] = pos;
     }
     std::vector<Applicant> applicants_copy = applicants;
-    std::sort(applicants_copy.begin(), applicants_copy.end(), [&](Applicant& left_applicant, Applicant& right_applicant) {
+    std::sort(
+        applicants_copy.begin(), applicants_copy.end(),
+        [&](Applicant& left_applicant, Applicant& right_applicant) {
         return (left_applicant > right_applicant);
     });
     std::vector<University> universities_copy = universities;
-    sort(universities_copy.begin(), universities_copy.end(), [&](University left_university, University right_university) {
+    sort(
+        universities_copy.begin(), universities_copy.end(),
+        [&](University left_university, University right_university) {
         return left_university.name > right_university.name;
     });
     AdmissionTable universities_students;
@@ -65,7 +70,8 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
     }
     for (Applicant& applicant : applicants_copy) {
         for (std::string& wished_university : applicant.wish_list) {
-            if (universities_student_count[university_position[wished_university]] == universities[university_position[wished_university]].max_students) {
+            if (universities_student_count[university_position[wished_university]] ==
+                universities[university_position[wished_university]].max_students) {
                 continue;
             }
             Student* p = &applicant.student;
@@ -75,7 +81,11 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
         }
     }
     for (auto& tmp : universities_students) {
-        std::sort(tmp.second.begin(), tmp.second.end());
+        std::sort(
+            tmp.second.begin(), tmp.second.end(),
+            [&](const Student* left_student, const Student* right_student){
+            return &left_student < &right_student;
+        });
     }
     return universities_students;
 }
