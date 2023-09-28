50fc2077076dff09105218a90149e9a838bce493
quinque <krivetskyy@icloud.com>
2023-09-29 01:47:08 +0300

    нормально

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,38 @@
 #include "scorer.h"
 
+#include <unordered_map>
+
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    return {}; // test
+    std::vector<const Event*> sorted_events;
+    for (const Event& event : events) {
+        sorted_events.push_back(&event);
+    }
+    std::sort(sorted_events.begin(), sorted_events.end(),
+              [](const Event* left_event, const Event* right_event) { return left_event->time < right_event->time; });
+    ScoreTable score_table;
+    std::map<StudentName, std::map<TaskName, bool>> students_tasks_status;
+    std::map<StudentName, std::set<TaskName>> students_merge_requests_status;
+    for (const Event* event : sorted_events) {
+        if (event->time > score_time) {
+            break;
+        }
+        if (event->event_type == EventType::CheckFailed) {
+            students_tasks_status[event->student_name][event->task_name] = 0;
+        } else if (event->event_type == EventType::CheckSuccess) {
+            students_tasks_status[event->student_name][event->task_name] = 1;
+        } else if (event->event_type == EventType::MergeRequestOpen) {
+            students_merge_requests_status[event->student_name].insert(event->task_name);
+        } else {
+            students_merge_requests_status[event->student_name].erase(event->task_name);
+        }
+    }
+    for (const auto& [student_name, student_tasks_status] : students_tasks_status) {
+        for (const auto& [task_name, task_status] : student_tasks_status) {
+            if (task_status && students_merge_requests_status[student_name].find(task_name) ==
+                                   students_merge_requests_status[student_name].end()) {
+                score_table[student_name].insert(task_name);
+            }
+        }
+    }
+    return score_table;
 }
