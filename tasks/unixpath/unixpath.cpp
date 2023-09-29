18ff16791d3c4d279190a301d67f9f343f50c08d
quinque <krivetskyy@icloud.com>
2023-09-30 02:49:23 +0300

    нормально

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,54 +1,38 @@
-#include <vector>
-
 #include "unixpath.h"
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    std::vector<std::string> last_dirs;
-    std::string current_dir;
-    for (size_t pos = 1; pos < current_working_dir.size(); ++pos) {
-        if (current_working_dir[pos] == '/') {
-            last_dirs.push_back(current_dir);
-            current_dir.clear();
-        } else {
-            current_dir += current_working_dir[pos];
-        }
-    }
-    if (!current_dir.empty()) {
-        last_dirs.push_back(current_dir);
-        current_dir.clear();
-    }
+    std::string full_path = static_cast<std::string>(current_working_dir);
     if (!path.empty() && path[0] == '/') {
-        last_dirs.clear();
+        full_path = "/";
     }
-    std::vector<std::string> minor_dirs;
-    for (size_t pos = 0; pos < path.size(); ++pos) {
-        if (path[pos] == '/') {
-            minor_dirs.push_back(current_dir);
-            current_dir.clear();
-        } else {
-            current_dir += path[pos];
-        }
-    }
-    minor_dirs.push_back(current_dir);
-    current_dir.clear();
-    for (std::string& dir : minor_dirs) {
-        if (dir.empty() || dir == ".") {
-            continue;
-        } else if (dir == "..") {
-            if (!last_dirs.empty()) {
-                last_dirs.pop_back();
+    full_path += "/";
+    full_path += static_cast<std::string>(path);
+    size_t full_path_pointer = 0;
+    full_path += "/";
+    for (size_t path_pointer = 0; path_pointer < full_path.size() - 1; ++path_pointer) {
+        if (full_path[path_pointer] == '/' && full_path[path_pointer + 1] == '.' &&
+            full_path[path_pointer + 2] == '.' && full_path[path_pointer + 3] == '/') {
+            while (full_path[full_path_pointer] != '/') {
+                --full_path_pointer;
             }
-        } else {
-            last_dirs.push_back(dir);
+            path_pointer += 2;
+        } else if (full_path[path_pointer] == '/' && full_path[path_pointer + 1] == '.' &&
+                   full_path[path_pointer + 2] == '/') {
+            ++path_pointer;
+            continue;
+        } else if (full_path[path_pointer + 1] != '/') {
+            ++path_pointer;
+            if (full_path[full_path_pointer] != '/') {
+                ++full_path_pointer;
+                full_path[full_path_pointer] = '/';
+            }
+            while (full_path[path_pointer] != '/') {
+                ++full_path_pointer;
+                full_path[full_path_pointer] = full_path[path_pointer];
+                ++path_pointer;
+            }
+            --path_pointer;
         }
     }
-    std::string full_path;
-    for (std::string& dir : last_dirs) {
-        full_path += "/";
-        full_path += dir;
-    }
-    if (full_path.empty()) {
-        full_path += "/";
-    }
-    return full_path;
+    return full_path.substr(0, full_path_pointer + 1);
 }
