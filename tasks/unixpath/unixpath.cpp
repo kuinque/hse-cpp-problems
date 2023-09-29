965817201e5f3f383b51bc48a0d464f8eb99d317
quinque <krivetskyy@icloud.com>
2023-09-29 03:07:31 +0300

    нормально

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -5,7 +5,7 @@
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
     std::vector<std::string> last_dirs;
     std::string current_dir;
-    for (int pos = 1; pos < current_working_dir.size(); ++pos) {
+    for (size_t pos = 1; pos < current_working_dir.size(); ++pos) {
         if (current_working_dir[pos] == '/') {
             last_dirs.push_back(current_dir);
             current_dir.clear();
@@ -15,28 +15,29 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
     }
     last_dirs.push_back(current_dir);
     current_dir.clear();
-    for (int pos = 0; pos < path.size(); ++pos) {
+    std::vector<std::string> minor_dirs;
+    for (size_t pos = 0; pos < path.size(); ++pos) {
         if (path[pos] == '/') {
-            if (!current_dir.empty()) {
-                last_dirs.push_back(current_dir);
-                current_dir.clear();
-            }
-            continue;
-        } else if ((pos == 0 || path[pos - 1] == '/') && path[pos] == '.' &&
-                   (pos + 1 == path.size() || path[pos + 1] != '.')) {
-            continue;
-        } else if ((pos == 0 || path[pos - 1] == '/') && path[pos] == '.') {
-            if (!last_dirs.empty()) {
-                last_dirs.pop_back();
-            }
-            ++pos;
+            minor_dirs.push_back(current_dir);
+            current_dir.clear();
         } else {
             current_dir += path[pos];
         }
     }
-    if (!current_dir.empty()) {
-        last_dirs.push_back(current_dir);
-        current_dir.clear();
+    minor_dirs.push_back(current_dir);
+    current_dir.clear();
+    for (std::string dir : minor_dirs) {
+        if (dir.empty() || dir == ".") {
+            continue;
+        }
+        else if (dir == "..") {
+            if (!last_dirs.empty()) {
+                last_dirs.pop_back();
+            }
+        }
+        else {
+            last_dirs.push_back(dir);
+        }
     }
     std::string full_path;
     for (std::string& dir : last_dirs) {
