c22c81bdadfd9b80c9f0b8e217cbecbb7ffd2ff9
quinque <krivetskyy@icloud.com>
2023-09-30 00:39:29 +0300

    нормально

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,20 +1,21 @@
-#include "tests_checking.h"
 #include <deque>
+
+#include "tests_checking.h"
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::deque<std::string> tests_stack;
+    std::deque<std::string> tests_deque;
     for (const StudentAction& student_action : student_actions) {
         if (student_action.side == Side::Bottom) {
-            tests_stack.push_back(student_action.name);
+            tests_deque.push_back(student_action.name);
         } else {
-            tests_stack.push_front(student_action.name);
+            tests_deque.push_front(student_action.name);
         }
     }
     std::vector<std::string> tests;
-    while (!tests_stack.empty()) {
-        tests.push_back(tests_stack.front());
-        tests_stack.pop_front();
+    while (!tests_deque.empty()) {
+        tests.push_back(tests_deque.front());
+        tests_deque.pop_front();
     }
     std::vector<std::string> queries_answers(queries.size());
     for (size_t query_number = 0; query_number < queries.size(); ++query_number) {
