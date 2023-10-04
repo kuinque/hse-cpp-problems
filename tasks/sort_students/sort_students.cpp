15039677ef1f3c3473b1d32391d388d3e54d6d33
quinque <krivetskyy@icloud.com>
2023-10-04 10:13:53 +0300

    нормально

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,60 +1,29 @@
 #include "sort_students.h"
 
 bool operator<(const Date& left_date, const Date& right_date) {
-    if (left_date.year < right_date.year) {
-        return true;
-    } else if (left_date.year > right_date.year) {
-        return false;
-    }
-    if (left_date.month < right_date.month) {
-        return true;
-    } else if (left_date.month > right_date.month) {
-        return false;
-    }
-    if (left_date.day < right_date.day) {
-        return true;
-    }
-    return false;
+    return std::tie(left_date.year, left_date.month, left_date.day) <
+           std::tie(right_date.year, right_date.month, right_date.day);
 }
 
 bool operator!=(const Date& left_date, const Date& right_date) {
-    return (left_date.day != right_date.day || left_date.month != right_date.month ||
-            left_date.year != right_date.year);
+    return std::tie(left_date.year, left_date.month, left_date.day) !=
+           std::tie(right_date.year, right_date.month, right_date.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
     auto comparator_by_date = [](const Student& left_student, const Student& right_student) -> bool {
-        if (left_student.birth_date < right_student.birth_date) {
-            return true;
-        } else if (left_student.birth_date != right_student.birth_date) {
-            return false;
-        }
-        if (left_student.last_name < right_student.last_name) {
-            return true;
-        } else if (left_student.last_name != right_student.last_name) {
-            return false;
-        }
-        if (left_student.name < right_student.name) {
-            return true;
-        } else {
-            return false;
-        }
+        std::tuple<const Date&, const std::string&, const std::string&> left_student_tuple =
+            std::tie(left_student.birth_date, left_student.last_name, left_student.name);
+        std::tuple<const Date&, const std::string&, const std::string&> right_student_tuple =
+            std::tie(right_student.birth_date, right_student.last_name, right_student.name);
+        return left_student_tuple < right_student_tuple;
     };
     auto comparator_by_name = [](const Student& left_student, const Student& right_student) -> bool {
-        if (left_student.last_name < right_student.last_name) {
-            return true;
-        } else if (left_student.last_name != right_student.last_name) {
-            return false;
-        }
-        if (left_student.name < right_student.name) {
-            return true;
-        } else if (left_student.name != right_student.name) {
-            return false;
-        }
-        if (left_student.birth_date < right_student.birth_date) {
-            return true;
-        }
-        return false;
+        std::tuple<const std::string&, const std::string&, const Date&> left_student_tuple =
+            std::tie(left_student.last_name, left_student.name, left_student.birth_date);
+        std::tuple<const std::string&, const std::string&, const Date&> right_student_tuple =
+            std::tie(right_student.last_name, right_student.name, right_student.birth_date);
+        return left_student_tuple < right_student_tuple;
     };
     if (sortKind == SortKind::Date) {
         std::sort(students.begin(), students.end(), comparator_by_date);
