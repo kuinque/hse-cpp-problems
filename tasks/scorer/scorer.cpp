331eb52da0e98c904efcde7598114b049749f0f4
quinque <krivetskyy@icloud.com>
2023-10-04 11:06:30 +0300

    нормально

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -16,14 +16,19 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         if (event->time > score_time) {
             break;
         }
-        if (event->event_type == EventType::CheckFailed) {
-            students_tasks_status[event->student_name][event->task_name] = false;
-        } else if (event->event_type == EventType::CheckSuccess) {
-            students_tasks_status[event->student_name][event->task_name] = true;
-        } else if (event->event_type == EventType::MergeRequestOpen) {
-            students_merge_requests_status[event->student_name].insert(event->task_name);
-        } else {
-            students_merge_requests_status[event->student_name].erase(event->task_name);
+        switch (event->event_type) {
+            case EventType::CheckFailed:
+                students_tasks_status[event->student_name][event->task_name] = false;
+                break;
+            case EventType::CheckSuccess:
+                students_tasks_status[event->student_name][event->task_name] = true;
+                break;
+            case EventType::MergeRequestOpen:
+                students_merge_requests_status[event->student_name].insert(event->task_name);
+                break;
+            case EventType::MergeRequestClosed:
+                students_merge_requests_status[event->student_name].erase(event->task_name);
+                break;
         }
     }
     for (const auto& [student_name, student_tasks_status] : students_tasks_status) {
