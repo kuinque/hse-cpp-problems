6aab02d89530736f08f594872fb8d1ac3b019124
quinque <krivetskyy@icloud.com>
2023-10-09 16:19:26 +0300

    lambda deleted

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -12,21 +12,17 @@ bool operator!=(const Date& left_date, const Date& right_date) {
            std::tie(right_date.year, right_date.month, right_date.day);
 }
 
+bool comparator_by_date(const Student& left_student, const Student& right_student) {
+    return std::tie(left_student.birth_date, left_student.last_name, left_student.name) <
+           std::tie(right_student.birth_date, right_student.last_name, right_student.name);
+}
+
+auto comparator_by_name(const Student& left_student, const Student& right_student) {
+    return std::tie(left_student.last_name, left_student.name, left_student.birth_date) <
+           std::tie(right_student.last_name, right_student.name, right_student.birth_date);
+}
+
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
-    auto comparator_by_date = [](const Student& left_student, const Student& right_student) -> bool {
-        std::tuple<const Date&, const std::string&, const std::string&> left_student_tuple =
-            std::tie(left_student.birth_date, left_student.last_name, left_student.name);
-        std::tuple<const Date&, const std::string&, const std::string&> right_student_tuple =
-            std::tie(right_student.birth_date, right_student.last_name, right_student.name);
-        return left_student_tuple < right_student_tuple;
-    };
-    auto comparator_by_name = [](const Student& left_student, const Student& right_student) -> bool {
-        std::tuple<const std::string&, const std::string&, const Date&> left_student_tuple =
-            std::tie(left_student.last_name, left_student.name, left_student.birth_date);
-        std::tuple<const std::string&, const std::string&, const Date&> right_student_tuple =
-            std::tie(right_student.last_name, right_student.name, right_student.birth_date);
-        return left_student_tuple < right_student_tuple;
-    };
     if (sortKind == SortKind::Date) {
         std::sort(students.begin(), students.end(), comparator_by_date);
     } else {
