34277e2f720f2609cea98a866f2ad4dfc7f9c341
quinque <krivetskyy@icloud.com>
2023-10-09 16:33:49 +0300

    style fixed

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -12,20 +12,20 @@ bool operator!=(const Date& left_date, const Date& right_date) {
            std::tie(right_date.year, right_date.month, right_date.day);
 }
 
-bool comparator_by_date(const Student& left_student, const Student& right_student) {
+bool CompareByDate(const Student& left_student, const Student& right_student) {
     return std::tie(left_student.birth_date, left_student.last_name, left_student.name) <
            std::tie(right_student.birth_date, right_student.last_name, right_student.name);
 }
 
-auto comparator_by_name(const Student& left_student, const Student& right_student) {
+auto CompareByName(const Student& left_student, const Student& right_student) {
     return std::tie(left_student.last_name, left_student.name, left_student.birth_date) <
            std::tie(right_student.last_name, right_student.name, right_student.birth_date);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
     if (sortKind == SortKind::Date) {
-        std::sort(students.begin(), students.end(), comparator_by_date);
+        std::sort(students.begin(), students.end(), CompareByDate);
     } else {
-        std::sort(students.begin(), students.end(), comparator_by_name);
+        std::sort(students.begin(), students.end(), CompareByName);
     }
 }
