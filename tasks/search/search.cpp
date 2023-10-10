ac46728add0a44b0667a3524c7787c7d8ac97441
quinque <krivetskyy@icloud.com>
2023-10-10 14:43:40 +0300

    sort fixed

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -6,38 +6,47 @@
 #include <sstream>
 
 std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
-    std::istringstream query_stream(static_cast<std::string>(query));
     std::set<std::string> query_words;
-    for (std::string s; getline(query_stream, s, ' ');) {
-        query_words.insert(s);
+    std::string word;
+    for (size_t i = 0; i <= query.size(); ++i) {
+        if (i < query.size() && std::isalpha(query[i])) {
+            word += std::tolower(static_cast<unsigned char>(query[i]));
+        } else {
+            if (!word.empty()) {
+                query_words.insert(word);
+            }
+            word.clear();
+        }
     }
     std::unordered_map<std::string, double> unique_word_count;
-    std::istringstream text_stream((static_cast<std::string>(text)));
     double line_count = 0;
-    for (std::string line; getline(text_stream, line, '\n');) {
+    for (size_t text_pointer = 0; text_pointer < text.size(); ++text_pointer) {
         ++line_count;
-        std::string word;
+        size_t next_text_pointer = text_pointer;
+        while (next_text_pointer < text.size() && text[next_text_pointer] != '\n') {
+            ++next_text_pointer;
+        }
+        std::string_view line = text.substr(text_pointer, next_text_pointer - text_pointer);
+        text_pointer = next_text_pointer;
+        word.clear();
         std::set<std::string> unique_words;
-        line += " ";
-        for (size_t i = 0; i < line.size(); ++i) {
-            if (std::isalpha(line[i])) {
-                word += static_cast<char>(std::tolower(line[i]));
+        for (size_t i = 0; i <= line.size(); ++i) {
+            if (i < line.size() && std::isalpha(line[i])) {
+                word += std::tolower(static_cast<unsigned char>(line[i]));
             } else {
                 if (!word.empty() && query_words.find(word) != query_words.end()) {
                     unique_words.insert(word);
                 }
-                word = "";
+                word.clear();
             }
         }
         for (const std::string& unique_word : unique_words) {
             ++unique_word_count[unique_word];
         }
     }
-    std::vector<std::string_view> answer_lines;
-    std::vector<size_t> index_answer_strings;
-    double best_relevant = 0;
-    text_stream.clear();
-    for (size_t text_pointer = 0; text_pointer < text.size(); ++text_pointer) {
+    std::vector<std::pair<double, size_t>> index_answer_strings;
+    std::set<std::tuple<double, size_t, std::string_view>> answer_strings_set;
+    for (size_t text_pointer = 0, line_number = 0; text_pointer < text.size(); ++text_pointer, ++line_number) {
         size_t next_text_pointer = text_pointer;
         while (next_text_pointer < text.size() && text[next_text_pointer] != '\n') {
             ++next_text_pointer;
@@ -45,11 +54,11 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         std::string_view line = text.substr(text_pointer, next_text_pointer - text_pointer);
         text_pointer = next_text_pointer;
         std::unordered_map<std::string, double> word_occurrence;
-        std::string word;
+        word = "";
         double word_count = 0;
         for (size_t i = 0; i < line.size(); ++i) {
             if (std::isalpha(line[i])) {
-                word += static_cast<char>(std::tolower(line[i]));
+                word += std::tolower(static_cast<unsigned char>(line[i]));
             } else {
                 if (!word.empty()) {
                     ++word_count;
@@ -72,13 +81,16 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
                 (word_occurrence[query_word] / word_count) * log(line_count / unique_word_count[query_word]);
             best_line_relevant = std::max(best_line_relevant, relevant);
         }
-        if (best_line_relevant > best_relevant) {
-            answer_lines.clear();
-            best_relevant = best_line_relevant;
+        if (best_line_relevant > 0) {
+            answer_strings_set.insert(std::make_tuple(best_line_relevant, SIZE_T_MAX - line_number, line));
         }
-        if (best_line_relevant > 0 && best_relevant == best_line_relevant && answer_lines.size() < results_count) {
-            answer_lines.push_back(line);
+        if (answer_strings_set.size() > results_count) {
+            answer_strings_set.erase(answer_strings_set.begin());
         }
+    }
+    std::vector<std::string_view> answer_lines;
+    for (auto iterator = answer_strings_set.begin(); iterator != answer_strings_set.end(); ++iterator) {
+        answer_lines.push_back(get<2>(*iterator));
     }
     return answer_lines;
 }
