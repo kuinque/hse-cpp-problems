7fcf7a66d43f9430aafdf3e3b1b8c02477d13abc
quinque <krivetskyy@icloud.com>
2023-10-11 19:05:41 +0300

    first solution

--- tasks/queue/queue.h
+++ tasks/queue/queue.h
@@ -5,7 +5,10 @@
 struct Node {
     int32_t value;
     Node* next;
-    Node(int32_t value, Node* next);
+    Node(int32_t value, Node* next) {
+        this->value = value;
+        this->next = next;
+    }
 };
 
 class Queue {
@@ -15,19 +18,64 @@ private:
     int32_t size_;
 
 public:
-    Queue();
+    Queue() {
+        front_ = nullptr;
+        tail_ = nullptr;
+        size_ = 0;
+    }
 
-    ~Queue();
+    ~Queue() {
+        while (front_ != nullptr) {
+            Node* tmp = front_->next;
+            delete front_;
+            front_ = tmp;
+        }
+        size_ = 0;
+    }
 
-    void Push(int32_t value);
+    void Push(int32_t value) {
+        ++size_;
+        Node* tmp = new Node(value, nullptr);
+        if (tail_ != nullptr) {
+            tail_->next = tmp;
+        }
+        tail_ = tmp;
+        if (front_ == nullptr) {
+            front_ = tail_;
+        }
+    }
 
-    void Pop();
+    void Pop() {
+        if (front_ == nullptr) {
+            return;
+        }
+        --size_;
+        Node* tmp = front_->next;
+        delete front_;
+        front_ = tmp;
+    }
 
-    int32_t Front() const;
+    int32_t Front() {
+        if (front_ == nullptr) {
+            return 0;
+        }
+        return front_->value;
+    }
 
-    int32_t Size() const;
+    int32_t Size() const {
+        return size_;
+    }
 
-    void Clear();
+    void Clear() {
+        while (front_ != nullptr) {
+            Node* tmp = front_->next;
+            delete front_;
+            front_ = tmp;
+        }
+        size_ = 0;
+    }
 
-    bool Empty() const;
+    bool Empty() {
+        return front_ == nullptr;
+    }
 };
