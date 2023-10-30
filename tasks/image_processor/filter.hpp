6376ccdbdc24bd5b3f24797bba23192070c5d476
quinque <krivetskyy@icloud.com>
2023-10-30 19:34:05 +0300

    first solve

--- tasks/image_processor/filter.hpp
+++ tasks/image_processor/filter.hpp
@@ -84,10 +84,12 @@ public:
         for (int32_t x = 0; x < height; ++x) {
             for (int32_t y = 0; y < width; ++y) {
                 Matrix::RGB rgb = pixel_array_->Get(x, y);
-                rgb.r = rgb.g = rgb.b =
-                    static_cast<uint8_t>(round(static_cast<double>(rgb.r) * get<0>(PIXEL_COEFFICIENT) +
-                                               static_cast<double>(rgb.g) * get<1>(PIXEL_COEFFICIENT) +
-                                               static_cast<double>(rgb.b) * get<2>(PIXEL_COEFFICIENT)));
+                uint8_t new_color = static_cast<uint8_t>(static_cast<double>(rgb.r) * get<0>(PIXEL_COEFFICIENT) +
+                                                         static_cast<double>(rgb.g) * get<1>(PIXEL_COEFFICIENT) +
+                                                         static_cast<double>(rgb.b) * get<2>(PIXEL_COEFFICIENT));
+                rgb.r = new_color;
+                rgb.g = new_color;
+                rgb.b = new_color;
                 temp.Set(x, y, rgb);
             }
         }
