953440a3594579467a5f69acc5ab8209bbbd6689
quinque <krivetskyy@icloud.com>
2023-11-17 16:36:01 +0300

    h files added

---
+++ tasks/image_processor/out/Debug/CMakeFiles/image_processor.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_processor.dir/blur.cpp.o"
+  "CMakeFiles/image_processor.dir/blur.cpp.o.d"
+  "CMakeFiles/image_processor.dir/crop.cpp.o"
+  "CMakeFiles/image_processor.dir/crop.cpp.o.d"
+  "CMakeFiles/image_processor.dir/edge.cpp.o"
+  "CMakeFiles/image_processor.dir/edge.cpp.o.d"
+  "CMakeFiles/image_processor.dir/filter.cpp.o"
+  "CMakeFiles/image_processor.dir/filter.cpp.o.d"
+  "CMakeFiles/image_processor.dir/greyscale.cpp.o"
+  "CMakeFiles/image_processor.dir/greyscale.cpp.o.d"
+  "CMakeFiles/image_processor.dir/image_processor.cpp.o"
+  "CMakeFiles/image_processor.dir/image_processor.cpp.o.d"
+  "CMakeFiles/image_processor.dir/input_controller.cpp.o"
+  "CMakeFiles/image_processor.dir/input_controller.cpp.o.d"
+  "CMakeFiles/image_processor.dir/negative.cpp.o"
+  "CMakeFiles/image_processor.dir/negative.cpp.o.d"
+  "CMakeFiles/image_processor.dir/retro.cpp.o"
+  "CMakeFiles/image_processor.dir/retro.cpp.o.d"
+  "CMakeFiles/image_processor.dir/sharp.cpp.o"
+  "CMakeFiles/image_processor.dir/sharp.cpp.o.d"
+  "image_processor"
+  "image_processor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_processor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
